101dae3ca89941666fe012b9fdef631c6f3b8a80
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-02-28 02:17:50 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import openai
 import re
-import os
 
-# Inicializa el modelo GPT-3
-openai.api_key = os.environ.get("OPENAI_API_KEY")
+# Pide la clave de API de OpenAI al usuario
+openai.api_key = st.text_input("Introduce tu clave de API de OpenAI:")
 
 # Función para contar palabras
 def count_words(text):
@@ -15,10 +14,11 @@ def count_words(text):
 def main():
     st.title("Generador de cuentos")
 
-    # Preguntamos al usuario por la sinopsis de la escena
+    # Preguntamos al usuario por la sinopsis del cuento
     prompt = st.text_area("Introduce la sinopsis del cuento que quieres generar:")
 
-    # Preguntamos por los personajes y su personalidad
+    # Preguntamos por detalles específicos sobre la trama y los personajes
+    genre = st.text_input("¿Qué género o temática te gustaría que tuviera el cuento?")
     character_count = st.number_input("¿Cuántos personajes hay en el cuento?", min_value=1, max_value=10, step=1)
     character_names = []
     character_roles = []
@@ -31,39 +31,42 @@ def main():
         character_roles.append(role)
         character_personalities.append(personality)
 
-    # Preguntamos por el género o temática del cuento
-    genre = st.selectbox("Selecciona el género o temática del cuento", ["Fantasía", "Ciencia ficción", "Romance", "Terror", "Misterio"])
-    
-if response.choices:
-    story_prompt = f"{response.choices[0].text}\n\nUna vez que {character_names[0]} {character_roles[0]}, {character_names[1]} {character_roles[1]}. "
-if has_dialogue == "Sí":
-    story_prompt += f"\"{st.text_input('Escribe una línea de diálogo:')}\" dijo {character_names[2]}."
-story_response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=story_prompt,
-    max_tokens=1600,
-    n=1,
-    stop=None,
-    temperature=0.7,
-)
+    # Preguntamos por el estilo o autor que se desea imitar y si hay diálogos
+    style = st.text_input("¿Qué estilo o autor te gustaría imitar?")
+    has_dialogue = st.radio("¿Incluye la escena algún diálogo?", ["Sí", "No"])
 
+    # Generamos el cuento con GPT-3
+    prompt = f"{prompt}\n\nGénero: {genre}\n\nPersonajes:\n"
+    for i in range(character_count):
+        prompt += f"{character_names[i]} es un {character_roles[i]} con una personalidad {character_personalities[i]}.\n"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=3600,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
 
     # Convertimos la escena en un cuento y mostramos el resultado
-story_prompt = f"{response.choices[0].text}\n\nUna vez que {character_names[0]} {character_roles[0]}, {character_names[1]} {character_roles[1]}. "
-story_prompt += f"El cuento es de {genre.lower()}."
-story_response = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=story_prompt,
-    max_tokens=1600,
-    n=1,
-    stop=None,
-    temperature=0.7,
-)
-
-# Mostramos el resultado final al usuario
-words = count_words(story_response.choices[0].text)
-st.write(f"Tu historia tiene {words} palabras:")
-st.write(story_response.choices[0].text)
+    if response.choices:
+        story_prompt = f"{response.choices[0].text}\n\nUna vez que {character_names[0]} {character_roles[0]}, {character_names[1]} {character_roles[1]}. "
+        if has_dialogue == "Sí":
+            story_prompt += f"\"{st.text_input('Escribe una línea de diálogo:')}\" dijo {character_names[2]}."
+        story_response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=story_prompt,
+            max_tokens=1600,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
+        # Mostramos el resultado final al usuario
+        words = count_words(story_response.choices[0].text)
+        st.write(f"Tu historia tiene {words} palabras:")
+        st.write(story_response.choices[0].text)
+    else:
+        st.write("Lo siento, no se pudo generar la historia. Por favor intenta con otra sinopsis o revisa la información que proporcionaste.")
 
 if __name__ == "__main__":
     main()
