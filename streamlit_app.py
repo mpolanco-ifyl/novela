86fcbb6d22bc6ac20d1635525c12d56d8dec5a9a
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-02-28 02:08:19 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ def main():
     
     if response.choices:
         story_prompt = f"{response.choices[0].text}\n\nUna vez que {character_names[0]} {character_roles[0]}, {character_names[1]} {character_roles[1]}. "
-        else:
+    else:
     # Manejar el caso en el que no hay respuesta del modelo
 
 
