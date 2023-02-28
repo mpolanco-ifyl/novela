08a2b17a85a7d53daa70356ec53ef5e6519d1d8d
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-02-27 23:26:04 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,53 +12,54 @@ def count_words(text):
 
 # Función principal de la aplicación
 def main():
-    st.title("Generador de escena de novela en español")
+    st.title("Generador de escenas de novela en español")
 
-    # Establecemos los valores de longitud de palabras para la escena de novela
-    word_count = 1600
+    # Preguntamos al usuario por la sinopsis de la escena
+    st.write("Escribe una sinopsis de la escena que te gustaría generar:")
+    prompt = st.text_area("Sinopsis:")
 
-    # Preguntamos al usuario por una sinopsis de la escena
-    st.write("Bienvenido al generador de escena de novela en español. Por favor, proporciona una breve sinopsis de la escena que te gustaría generar:")
-    prompt = st.text_area("Escribe la sinopsis aquí:")
-
-    # Preguntamos por los personajes, si es necesario
-    has_characters = st.radio("¿La escena incluye personajes?", ["Sí", "No"])
-    if has_characters == "Sí":
-        character_count = st.number_input("¿Cuántos personajes hay en la escena?", min_value=1, max_value=10, step=1)
-
-        # Preguntamos por los nombres, roles y personalidades de cada personaje
-        character_names = []
-        character_roles = []
-        character_personalities = []
-        for i in range(character_count):
-            name = st.text_input(f"Nombre del personaje #{i+1}")
-            role = st.text_input(f"Papel del personaje #{i+1}")
-            personality = st.text_area(f"Personalidad del personaje #{i+1}")
-            character_names.append(name)
-            character_roles.append(role)
-            character_personalities.append(personality)
+    # Preguntamos por los personajes
+    character_count = st.number_input("¿Cuántos personajes hay en la escena?", min_value=1, max_value=10, step=1)
+    character_names = []
+    character_roles = []
+    character_personalities = []
+    for i in range(character_count):
+        name = st.text_input(f"Nombre del personaje #{i+1}")
+        role = st.text_input(f"Papel del personaje #{i+1}")
+        personality = st.text_area(f"Personalidad de {name}:")
+        character_names.append(name)
+        character_roles.append(role)
+        character_personalities.append(personality)
 
     # Preguntamos por el estilo o autor que se desea imitar
-    style = st.text_input("¿Qué estilo o autor te gustaría imitar en la escena?")
+    author_style = st.text_input("¿Qué estilo o autor te gustaría imitar? (opcional)")
 
-    # Preguntamos si la escena tiene diálogos
-    has_dialogue = st.radio("¿La escena tiene diálogos?", ["Sí", "No"])
+    # Preguntamos si hay diálogos
+    has_dialogue = st.radio("¿Incluye diálogos la escena?", ["Sí", "No"])
 
-    # Generamos el contenido con GPT-3
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=word_count,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
+    # Generamos la escena al presionar el botón
+    if st.button("Generar escena"):
+        prompt += f"\n\nPersonajes: {', '.join(character_names)}"
+        for i in range(character_count):
+            prompt += f"\n\n{character_names[i]} es {character_roles[i]} y tiene una personalidad {character_personalities[i]}."
+        if has_dialogue == "Sí":
+            prompt += "\n\nDiálogo: [Inserta aquí el diálogo de la escena]"
+        if author_style:
+            prompt += f"\n\nEstilo o autor a imitar: {author_style}"
 
-    # Contamos las palabras en la respuesta y la mostramos al usuario
-    words = count_words(response.choices[0].text)
-    if words < word_count:
-        response.choices[0].text += " " * (word_count - words)
-    st.write(response.choices[0].text)
+        # Generamos la escena con GPT-3
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            max_tokens=3048,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
+
+        # Contamos las palabras en la respuesta y la mostramos al usuario
+        words = count_words(response.choices[0].text)
+        st.write(response.choices[0].text)
 
     # Mostramos el botón de reiniciar para que el usuario pueda volver a empezar
     if st.button("Reiniciar"):
