48ea27c6e44fd32e4de78b0cab24838f7955a47a
mpolanco-ifyl <123343881+mpolanco-ifyl@users.noreply.github.com>
2023-02-27 23:24:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,29 @@ def main():
     st.write("Bienvenido al generador de escena de novela en español. Por favor, proporciona una breve sinopsis de la escena que te gustaría generar:")
     prompt = st.text_area("Escribe la sinopsis aquí:")
 
+    # Preguntamos por los personajes, si es necesario
+    has_characters = st.radio("¿La escena incluye personajes?", ["Sí", "No"])
+    if has_characters == "Sí":
+        character_count = st.number_input("¿Cuántos personajes hay en la escena?", min_value=1, max_value=10, step=1)
+
+        # Preguntamos por los nombres, roles y personalidades de cada personaje
+        character_names = []
+        character_roles = []
+        character_personalities = []
+        for i in range(character_count):
+            name = st.text_input(f"Nombre del personaje #{i+1}")
+            role = st.text_input(f"Papel del personaje #{i+1}")
+            personality = st.text_area(f"Personalidad del personaje #{i+1}")
+            character_names.append(name)
+            character_roles.append(role)
+            character_personalities.append(personality)
+
+    # Preguntamos por el estilo o autor que se desea imitar
+    style = st.text_input("¿Qué estilo o autor te gustaría imitar en la escena?")
+
+    # Preguntamos si la escena tiene diálogos
+    has_dialogue = st.radio("¿La escena tiene diálogos?", ["Sí", "No"])
+
     # Generamos el contenido con GPT-3
     response = openai.Completion.create(
         engine="text-davinci-003",
